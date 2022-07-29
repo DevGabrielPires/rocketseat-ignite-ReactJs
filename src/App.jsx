065c24fe879489788fad43a982aeab596c1516e2
DevGabrielPires <gabriel.r.pires@outlook.com>
2022-07-28 21:55:57 -0300

    :sparkles: Init: Init project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Post } from './Post'
+
+
+function App() {
+
+  return (
+    <>
+      <Post 
+        author="Gabriel Pires" 
+        content="
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi ipsum aliquid unde, ratione commodi a laborum amet suscipit mollitia vel? Distinctio quos incidunt harum dicta, temporibus obcaecati ut consequuntur quo!" 
+      />
+      <Post 
+        author="Franciele Rodrigues" 
+        content="
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi ipsum aliquid unde, ratione commodi a laborum amet suscipit mollitia vel? Distinctio quos incidunt harum dicta, temporibus obcaecati ut consequuntur quo!" 
+      />
+
+
+    </>
+  )
+}
+
+
+export default App
